ea71a07c21d3b7e2320359560bcc802c0aa6a238
Libritho <65232465+Libritho@users.noreply.github.com>
2020-08-12 18:03:29 +0300

    Code cleanup

--- test.js
+++ test.js
@@ -2,24 +2,20 @@ window.onload = addListeners;
 
 function addListeners () {
     let description = document.getElementsByClassName("description");
-    let slides = document.getElementsByClassName("slides");
+    let category = document.getElementsByClassName("relative");
     let arrows = document.getElementsByClassName("arrow");
 
     for (let i = 0; i<description.length; i++) {
-        slides[i].onclick = function () {
-            slides[i].className += " active";
+        category[i].onclick = function () {
+            description[i].className += " active";
             description[i].style.display = "flex";
             arrows[i].className = "arrow active"
-
 
             for (let z = 0; z<description.length; z++)
                 if (z !== i) {
                     {
-
-                        slides[z].className = "slides";
                         description[z].style.display = "none";
                         arrows[z].className = "arrow";
-
                     }
                 }
         }
